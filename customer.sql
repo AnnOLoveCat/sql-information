131880f71efd7b764b6a568eddd534914cf346d4
AnnuLoveCat <gordon.kao118@gmail.com>
2022-07-02 10:41:22 +0800

    Create New Table

---
+++ customer.sql
@@ -0,0 +1,12 @@
+create table customer_list(
+	customer_id int not null,
+    name varchar(20),
+    major varchar(20),
+	primary key(customer_id)
+);
+#Create A New Table = create table <Type Your Name Of list> 
+#In "customer_list" (<Here>) Type You Field's Name
+#Define This Field Is Primary Key Or Just Define This Data's Type 
+
+describe customer;
+select * from customer_list
